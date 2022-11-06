aaaa593b9444f8d0f378506f41f091e10778b8a8
nilesh <deorenilesh@kbtcoe.org>
2022-11-06 15:03:04 +0530

    underline

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className='bg-blue-700 '>This is the home page of Portfio website</h1>
+      <h1 className='bg-blue-700 underline'>This is the home page of Portfio website</h1>
     </div>
   )
 }
